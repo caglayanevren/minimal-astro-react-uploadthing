47f3aed8747857475a27f45142c5c0038a3e9118
Evren Çağlayan <caglayanevren@gmail.com>
2024-09-17 11:43:45 +0300

    astro config delete base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/',
     integrations: [react()],
     output: 'hybrid',
     adapter: nodejs({
